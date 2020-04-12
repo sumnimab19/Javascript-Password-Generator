04f0fe526b7ddca40888f357d9093d37c98a7e89
Sumnima B <sumnimabhandari@gmail.com>
2020-04-12 15:40:44 -0700

    Updated the logic for entire code

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function writePassword() {
 }
 
 function generatePassword(){
+  var lowerChar = ["abcdefghijklmnopqrstuvwxyz"];
+  var upperChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  var numChar = [1234567890];
+  var specialChar = ["!#$%&'()*+-./:;<=>?@"];
+  var randomString="";
 
   var inputLength = prompt("Enter length of password between 8 - 128 characters.");
   var isInputLower = confirm("Do you want to include Lower Case characters?");
@@ -19,29 +24,18 @@ function generatePassword(){
   var isInputNumber = confirm("Do you want to include Number?");
   var isInputSpecialChar = confirm("Do you want to include Special Characters?")
 
-  if (isInputLower){
-    var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var inputLower = lowerChar[Math.floor(Math.random() * lowerChar.length)];
-    console.log(inputLower);
+  
+  if ((isInputLower) && (isInputUpper) && (isInputNumber) && (isInputSpecialChar)) {
+    var newString = lowerChar + upperChar + numChar + specialChar; 
+    console.log(newString);
   } 
-  if (isInputUpper) {
-    var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    var inputUpper = upperChar[Math.floor(Math.random() * upperChar.length)];
-    console.log(inputUpper);
-  }
-  if (isInputNumber) {
-    var numChar = [0,1,2,3,4,5,6,7,8,9];
-    var inputNumber = numChar[Math.floor(Math.random() * numChar.length)];
-    console.log(inputNumber);
-  }
-  if (isInputSpecialChar) {
-    var specialChar = ["!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@"];
-    var inputSpecialChar = specialChar[Math.floor(Math.random() * specialChar.length)];
-    console.log(inputSpecialChar);
+
+  for (var i = 0; i < inputLength; i++) {
+    var newPassword = newString[Math.floor(Math.random() * newString.length)];
+    randomString+= newPassword;
   }
 
-  return (inputLower + inputUpper + inputNumber + inputSpecialChar);
-  console.log(NewPassword);
+  return (randomString);
 
 }
 // Add event listener to generate button
@@ -64,3 +58,4 @@ generateBtn.addEventListener("click", writePassword);
 //THEN a password is generated that matches the selected criteria
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page
+
