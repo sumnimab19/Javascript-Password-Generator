c283313f2eec0da47baee567fcfb5f2500027dd6
Sumnima B <sumnimabhandari@gmail.com>
2020-04-16 17:41:40 -0700

    Added ReadMe content. Cleaned the comments

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Javascript-Password-Generator
+
+This application generates a random password based on user-selected criteria. This app runs in the browser and feature dynamically updated HTML and CSS powered by JavaScript. 
+
+
+## Description
+
+User is presented with the Password Generator card with a ‘Generate Password’ red button. When the user clicks on the button, they are prompted for the length of the password. This application validates the user input for these conditions:
+	1. Number between 8 - 128
+	2. Float numbers
+	3. Any non numeric inputs
+	4. Null input 
+
+
+Then users are prompted with these character type inputs:
+	1. Lower Case
+	2. Upper Case
+	3. Number
+	4. Special Character
+
+It then validates that the user must select at least one character type to proceed. 
+
+Once at least one character type is selected, password is generated that matches the selected criteria and a random password is displayed on the page.
+
+
+## Technology Used
+1. Semantic HTML5
+2. JavaScript
+3. CSS
+
+
+## Screenshot
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var colArr = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -16,20 +15,18 @@ function writePassword() {
 function generatePassword() {
   
   var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  
   var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
   var numChar = [1,2,3,4,5,6,7,8,9,0];
-
   var specialChar = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
   
-  var newPassword="";
+  var newPassword = "";
+  var newString;
 
   // User prompted for the length of the password.
   var inputLength = prompt("Enter length of password needed between 8 - 128.");
   
   // Password length less than 8 characger, greater than 128 character and anything other than numerical value is invalid input. 
-  if((inputLength < 8) || (inputLength > 128) || (isNaN(inputLength))){
+  if((inputLength < 8) || (inputLength > 128) || (isNaN(inputLength) || (!isNaN(inputLength) && inputLength.toString().indexOf('.') != -1))){
     alert("'" + inputLength + "'" + " is Invalid Input. Please enter a number between 8 and 128 to generate a valid password.");
   }
   else {
@@ -45,11 +42,13 @@ function generatePassword() {
     } else{
 
       // Conditional statements to generate password that matches the selected criteria.
-      // When all inputs are selected as 'OK'
+
+      // --- When all 4 inputs are selected ---
       if ((isInputLower) && (isInputUpper) && (isInputNumber) && (isInputSpecialChar)) {
-        var newString = lowerChar.join("") + numChar.join("") + upperChar.join("") + specialChar.join("");
+        newString = lowerChar.join("") + numChar.join("") + upperChar.join("") + specialChar.join("");
         
-        // When 3 out of 4 inputs are selected as 'OK'
+        
+        // --- When 3 out of 4 inputs are selected ---
       } else if ((isInputLower) && (isInputUpper) && (isInputNumber)) {
           newString = lowerChar.join("") + upperChar.join("") + numChar.join("");
       } else if ((isInputLower) && (isInputUpper) && (isInputSpecialChar)) {
@@ -59,7 +58,7 @@ function generatePassword() {
       } else if ((isInputUpper) && (isInputNumber) && (isInputSpecialChar)) {
           newString = upperChar.join("") + numChar.join("") + specialChar.join("");
 
-        // When 2 out of 4 inputs are selected as 'OK'
+        // --- When 2 out of 4 inputs are selected ---
       } else if ((isInputLower) && (isInputUpper)) {
           newString = lowerChar.join("") + upperChar.join("");
       } else if ((isInputLower) && (isInputNumber)) {
@@ -73,7 +72,7 @@ function generatePassword() {
       } else if ((isInputNumber) && (isInputSpecialChar)) {
           newString = numChar.join("") + specialChar.join("");
 
-        // When 1 out of 4 input is selected as 'OK'
+        // --- When 1 out of 4 input is selected ---
       } else if (isInputLower) {
           newString = lowerChar.join("");
       } else if (isInputUpper) {
@@ -84,7 +83,7 @@ function generatePassword() {
           newString = specialChar.join("");
       } 
 
-      // Loop to generate password based on length input.
+      // Loop to generate password based on length of the input.
       for (var i = 0; i < inputLength; i++) {
         var randomString = newString[Math.floor(Math.random() * newString.length)];
         newPassword+= randomString;
